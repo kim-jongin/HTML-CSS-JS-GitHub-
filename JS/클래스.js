115c8bd403a4d390a33319d238bee49e430b0db9
kim-jongin <whddlskss@gmail.com>
2023-02-25 23:42:38 +0900

    [클래스] 필드

--- JS/클래스.js
+++ JS/클래스.js
@@ -1,5 +1,5 @@
 //클래스를 사용하여 인스턴스 만들기
-class YalcoChicken {
+class JongChicken {
   constructor (name, no) {
     this.name = name;
     this.no = no;
@@ -9,9 +9,9 @@ class YalcoChicken {
   }
 }
 
-const chain1 = new YalcoChicken('판교', 3);
-const chain2 = new YalcoChicken('강남', 17);
-const chain3 = new YalcoChicken('제주', 24);
+const chain1 = new JongChicken('판교', 3);
+const chain2 = new JongChicken('강남', 17);
+const chain3 = new JongChicken('제주', 24);
 
 console.log(chain1, chain1.introduce());
 console.log(chain2, chain2.introduce());
@@ -22,9 +22,9 @@ console.log(chain3, chain3.introduce());
 
 //그러나 클래스와 생성자 함수의 동작이 동일하지는 않음
 // 차이 1. 클래스는 호이스팅되지 않음 (정확히는 되지만...)
-const chain1 = new YalcoChicken('판교', 3);
+const chain1 = new JongChicken('판교', 3);
 
-class YalcoChicken {
+class JongChicken {
   constructor (name, no) {
     this.name = name;
     this.no = no;
@@ -36,7 +36,7 @@ class YalcoChicken {
 
 // 차이 2. 클래스는 new 없이 사용하면 오류
 // (생성자 함수는 오류 없이 undefined 반환)
-const chain2 = YalcoChicken('강남', 17);
+const chain2 = JongChicken('강남', 17);
 //이 외에도 차이들이 있음 - 클래스에는 이후 배울 엄격모드 적용
 
 //constructor 메서드 
@@ -84,3 +84,49 @@ function Dog2 () {
 const badugi = new Dog2();
 console.log(badugi, badugi.bark());
 
+//필드
+// constructor 밖에서, this.~ 없이 인스턴스의 프로퍼티 정의
+// 2022 말 아직은 제안사항, 이미 다수 브라우저에서 지원
+// 이후 배울 Babel로 해결 가능
+
+// 필드값이 지정되어 있으므로 constructor 메서드 필요없음
+class Slime {
+  hp = 50;
+  op = 4;
+  attack (enemy) {
+    enemy.hp -= this.op;
+    this.hp += this.op/4;
+  }
+}
+
+const slime1 = new Slime();
+const slime2 = new Slime();
+
+console.log(slime1, slime2);
+slime1.attack(slime2);
+console.log(slime1, slime2);
+
+class JongChicken {
+  no = 0;
+  menu = { '후라이드': 10000, '양념치킨': 12000 };
+
+  constructor (name, no) {
+    this.name = name;
+    if (no) this.no = no;
+  }
+  introduce () {
+    return `안녕하세요, ${this.no}호 ${this.name}점입니다!`;
+  }
+  order (name) {
+    return `${this.menu[name]}원입니다.`
+  }
+}
+const chain0 = new JongChicken('(미정)');
+console.log(chain0, chain0.introduce());
+
+const chain1 = new JongChicken('판교', 3);
+console.log(chain1, chain1.introduce());
+
+chain1.menu['양념치킨'] = 13000;
+
+console.log(chain0.order('양념치킨'), chain1.order('양념치킨'));
