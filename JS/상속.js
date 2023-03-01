8680a90bb47666d6f85553cfeab0bb7e8456337a
kim-jongin <whddlskss@gmail.com>
2023-03-01 23:31:13 +0900

    [상속] 오버라이딩

--- JS/상속.js
+++ JS/상속.js
@@ -71,3 +71,56 @@ console.log(
   chain1.cafeOrder('라떼')
 );
 
+//오버라이딩 overriding -> 자식 클래스에서 부모로부터 물려받은 속성이나 기능을 덮어씀
+class Bird {
+  wings = 2;
+  canFly = true;
+  travel () { console.log('비행중...') }
+}
+class Eagle extends Bird {
+  claws = 2;
+}
+class Penguin extends Bird {
+  canFly = false;
+  travel () { console.log('수영중...') }
+}
+
+const eaglee = new Eagle();
+const pengu = new Penguin();
+
+console.log(eaglee);
+eaglee.travel();
+
+console.log(pengu);
+pengu.travel();
+
+class JongChicken {
+  no = 0;
+  menu = { '후라이드': 10000, '양념치킨': 12000 };
+
+  constructor (name, no) {
+    this.name = name;
+    if (no) this.no = no;
+  }
+  introduce () {
+    return `안녕하세요, ${this.no}호 ${this.name}점입니다!`;
+  }
+  order (name) {
+    return `${this.menu[name]}원입니다.`
+  }
+}
+
+class YorkNannyJongChicken extends JongChicken {
+  introduce () {
+    return `또 뭐 쳐먹으러 기어들어왔어.`;
+  }
+  order (name) {
+    return `${this.menu[name]}원이여.`
+  }
+}
+
+const chain1 = new YorkNannyJongChicken ('종로', 48);
+
+console.log(chain1.introduce());
+
+console.log(chain1.order('양념치킨'));
