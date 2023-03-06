e1350a9bc4429fda42502db3cd711f27c68e3bc5
kim-jongin <whddlskss@gmail.com>
2023-03-06 23:36:47 +0900

    [Number객체]

---
+++ JS/Number객체.js
@@ -0,0 +1,13 @@
+//생성자 함수
+const numObj1 = new Number();
+const numObj2 = new Number(123);
+const numObj3 = new Number(-123.45);
+
+console.log(numObj1, numObj2, numObj3);
+
+console.log(
+  numObj1.valueOf(),
+  numObj2.valueOf(),
+  numObj3.valueOf()
+);
+
